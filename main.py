92b8b409a6de4cacffa4bf1ea18f37ebe1c66eb1
Reno DuBois <rdubois96@gmail.com>
2016-09-30 01:21:48 -0500

    added player class and gitignore

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
+import trueskill
+from player import Player
+
 if __name__ == "__main__":
-    pass
+    p1 = trueskill.Rating()
+    p2 = trueskill.Rating()
+    print(p1, p2)
+    p1, p2 = trueskill.rate_1vs1(p1, p2)
+    print(p1, p2)
---
+++ player.py
@@ -0,0 +1,11 @@
+from trueskill import Rating
+
+class Player:
+    '''
+    default constructor for Player class
+    tag - string that is the player's in-game-name
+    rating - int that represents their Trueskill rating
+    '''
+    def __init__(self, tag, rating):
+        self.tag = tag
+        self.rating = Rating(rating)
