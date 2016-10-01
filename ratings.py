276de0f61e993a9a59d6289393f4e97aa49a5a21
renodubois <rtdm9c@mst.edu>
2016-10-01 00:09:56 -0500

    added sorted_ratings() to return for the website

--- ratings.py
+++ ratings.py
@@ -1,8 +1,13 @@
 import json
+import csv
 import trueskill
 from player import Player
 
-
+'''
+    takes a player file and a tournament file and updates the player's ratings based on the tournament results
+    player_file: filepath to a .json file formatted for tournament-seeder
+    tournament_file: filepath to a .csv results file that updates ratings
+'''
 def update_ratings(player_file, tournament_file):
     # temporary tournament file for test, will use command line later
     # let's import our json of players and their ratings
@@ -17,8 +22,11 @@ def update_ratings(player_file, tournament_file):
 
 
     # Grab a tournament file, read the results and update the players' ratings
+    tourney_data = []
     with open(tournament_file) as raw_data:
-        tourney_data = json.load(raw_data)
+        reader = csv.reader(raw_data)
+        for row in reader:
+            tourney_data.append(row)
 
     # Go through given tournament and update ratings
     for match in tourney_data:
@@ -26,15 +34,15 @@ def update_ratings(player_file, tournament_file):
         p1_index = 0
         p2_index = 0
         for i in range(0, len(players)):
-            if match['p1'] == players[i].tag:
+            if match[0] == players[i].tag:
                 p1_index = i
-            elif match['p2'] == players[i].tag:
+            elif match[1] == players[i].tag:
                 p2_index = i
         # Player 1 was the winner
-        if match['result'] == 0:
+        if int(match[2]) == 0:
             # Run TrueSkill
             players[p1_index].rating, players[p2_index].rating = trueskill.rate_1vs1(players[p1_index].rating, players[p2_index].rating)
-        # Player 1 was the winner
+        # Player 2 was the winner
         else:
             # Run TrueSkill
             players[p2_index].rating, players[p1_index].rating = trueskill.rate_1vs1(players[p2_index].rating, players[p1_index].rating)
@@ -44,3 +52,18 @@ def update_ratings(player_file, tournament_file):
         json_players.append({'Rating':player.rating.mu, 'Tag':player.tag})
     with open(player_file, 'w') as file:
         json.dump(json_players, file, sort_keys=True)
+
+'''
+    takes a player file and returns a list sorted by rating
+    player_file: filepath to a .json formatted for tournament-seeder
+'''
+def sorted_ratings(player_file):
+    # Open our player file and grab our players and ratings
+    with open(player_file) as file:
+        player_data = json.load(file)
+    players = []
+    for plr in player_data:
+        print(plr)
+        rating_float = float(plr['Rating'])
+        players.append(Player(plr['Tag'], rating_float))
+    return sorted(players, reverse=True)
