7026238d44bbaea7a72059097ce97f4cd22bba26
renodubois <rtdm9c@mst.edu>
2016-10-07 01:44:51 -0500

    restructured project

--- setup.py
+++
@@ -1,9 +0,0 @@
-# User, password, host, database
-
-with open("MeleeCharacters.txt") as meleeFile:
-    meleeCharacters = meleeFile.read()
-    meleeCharacters = meleeCharacters.splitlines()
-
-with open("Admins.txt") as adminFile:
-    admins = adminFile.read()
-    admins = admins.splitlines()
