2e0990e339e561209723677ef0a13dae02b0a98f
Reno DuBois <renodubois@users.noreply.github.com>
2016-10-10 18:58:42 -0500

    Delete setup.py

--- config/setup.py
+++
@@ -1,9 +0,0 @@
-# User, password, host, database
-
-with open("config/MeleeCharacters.txt") as meleeFile:
-    meleeCharacters = meleeFile.read()
-    meleeCharacters = meleeCharacters.splitlines()
-
-with open("config/Admins.txt") as adminFile:
-    admins = adminFile.read()
-    admins = admins.splitlines()
